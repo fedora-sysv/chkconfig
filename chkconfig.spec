dcd74508feae230f42a699495c66a4b478c90e9d
Bill Nottingham <notting@redhat.com>
2005-02-10 23:13:49 +0000

    backport fix here too

--- chkconfig.spec
+++ chkconfig.spec
@@ -1,7 +1,7 @@
 # [for (x)emacs] -*-  mode: RPM-SPEC; coding: utf-8 -*-
 Summary: A system tool for maintaining the /etc/rc*.d hierarchy.
 Name: chkconfig
-Version: 1.3.13
+Version: 1.3.13.1
 Release: 0.2.1
 License: GPL
 Group: System Environment/Base
@@ -75,6 +75,10 @@ rm -rf $RPM_BUILD_ROOT
 %{_mandir}/*/ntsysv.8*
 
 %changelog
+* Thu Feb 10 2005 Bill Nottingham <notting@redhat.com> 1.3.13.1-0.2.1
+- fix: prefer chkconfig: priorities if Required-Start/Required-Stop
+  aren't set
+
 * Tue Jan 11 2005 Bill Nottingham <notting@redhat.com> 1.3.13-0.2.1
 - fix LSB comment parsing some more (#144739)
 
--- leveldb.c
+++ leveldb.c
@@ -237,8 +237,8 @@ int readServiceInfo(char * name, struct service * service, int honorHide) {
     struct service serv = { 
 	    	    name: NULL, 
 		    levels: -1, 
-		    kPriority: -1, 
-		    sPriority: -1, 
+		    kPriority: -2, 
+		    sPriority: -2, 
 		    desc: NULL, 
 		    startDeps: NULL, 
 		    stopDeps: NULL,
@@ -295,13 +295,8 @@ int readServiceInfo(char * name, struct service * service, int honorHide) {
 	if (*start != '#') continue;
 
 	start++;
-	if (!strncmp(start, "## BEGIN INIT INFO", 18)) {
+	if (!strncmp(start, "## BEGIN INIT INFO", 18))
 		    serv.isLSB = 1;
-		    serv.sPriority = -1;
-		    serv.kPriority = -1;
-	}
-	if (!strncmp(start, "## END INIT INFO", 16) && serv.isLSB)
-		    break;
 		
 	while (isspace(*start) && start < end) start++;
 	if (start == end) continue;
@@ -333,9 +328,11 @@ int readServiceInfo(char * name, struct service * service, int honorHide) {
 		free(bufstart);
 		return 1;
 	    }
-	    if (!serv.isLSB) {
-		    serv.sPriority = spri;
-		    serv.kPriority = kpri;
+	    if (serv.sPriority == -2 || serv.kPriority == -2) {
+		    if (serv.sPriority == -2)
+			    serv.sPriority = spri;
+		    if (serv.kPriority == -2)
+			    serv.kPriority = kpri;
 	    }
 
 	    if (!strcmp(levelbuf, "-"))
@@ -385,6 +382,8 @@ int readServiceInfo(char * name, struct service * service, int honorHide) {
 		char *t;
 		int numdeps = 0;
 		
+
+		serv.sPriority = -1;
 		start+=15;
 		while (1) {
 			while (*start && isspace(*start) && start < end) start++;
@@ -410,6 +409,7 @@ int readServiceInfo(char * name, struct service * service, int honorHide) {
 		char *t;
 		int numdeps = 0;
 		
+		serv.kPriority = -1;
 		start+=14;
 		while (1) {
 			while (*start && isspace(*start) && start < end) start++;
