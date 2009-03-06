4b9be1af164f0a391e7375914294179753942787
Bill Nottingham <notting@redhat.com>
2009-03-06 01:14:06 -0500

    LSB-only services can have start & stop priorities not set - it's not an error.

--- leveldb.c
+++ leveldb.c
@@ -618,7 +618,7 @@ int parseServiceInfo(int fd, char * name, struct service * service, int honorHid
     } else if (english_desc)
 	free (english_desc);
 
-    if (!partialOk && ((serv.levels == -1) || !serv.desc || serv.sPriority == -1 || serv.kPriority == -1)) {
+    if (!partialOk && ((serv.levels == -1) || !serv.desc || (!serv.isLSB && (serv.sPriority == -1 || serv.kPriority == -1)))) {
 	return 1;
     } 
 
