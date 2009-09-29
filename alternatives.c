976185deed8ec19fc0e6f9e8ca86d3130a3f8770
Bill Nottingham <notting@redhat.com>
2009-09-29 16:22:16 -0400

    Clarify some error messages with more context. (#441443)

--- alternatives.c
+++ alternatives.c
@@ -243,6 +243,7 @@ static int readConfig(struct alternativeSet * set, const char * title,
 
 	if (*line != '/') {
 	    fprintf(stderr, _("path to alternate expected in %s\n"), path);
+	    fprintf(stderr, _("unexpected line in %s: %s\n"), path, line);
 	    return 1;
 	}
 
@@ -263,6 +264,7 @@ static int readConfig(struct alternativeSet * set, const char * title,
 	set->alts[set->numAlts].initscript = NULL;
 	if (!end || (end == line)) {
 	    fprintf(stderr, _("numeric priority expected in %s\n"), path);
+	    fprintf(stderr, _("unexpected line in %s: %s\n"), path, line);
 	    return 1;
 	}
 	if (end) {
@@ -285,6 +287,7 @@ static int readConfig(struct alternativeSet * set, const char * title,
 	    line = parseLine(&buf);
 	    if (line && strlen(line) && *line != '/') {
 		fprintf(stderr, _("slave path expected in %s\n"), path);
+		fprintf(stderr, _("unexpected line in %s: %s\n"), path, line);
 		return 1;
 	    }
 
