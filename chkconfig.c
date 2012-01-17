3c9dcf4bf26658ebea02cc942ca1c859de8efcc4
Bill Nottingham <notting@redhat.com>
2012-01-17 11:47:06 -0500

    If merely passed a service to check enablement, make sure to initialize the current runlevel if it's not specified. (#782152)

--- chkconfig.c
+++ chkconfig.c
@@ -799,8 +799,11 @@ int main(int argc, const char ** argv) {
 		       return !s.levels;
 	       else
 		       return 1;
-	    } else
+	    } else {
+               if (level == -1)
+                   level = currentRunlevel();
 	       return s.currentLevels & (1 << level) ? 0 : 1;
+	    }
 	} else if (!strcmp(state, "on")) {
 	    if (!noRedirectItem) {
 		forwardSystemd(name, type, "enable");
