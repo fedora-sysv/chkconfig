30892e2732596240ced10f7519a4fb9eac127751
Marcel Plch <mplch@redhat.com>
2018-09-06 18:10:22 +0200

    Add tests for --add/remove-slave and use beakerlib

--- test-alternatives.sh
+++ test-alternatives.sh
@@ -1,19 +1,13 @@
 #!/bin/bash
 
-[ -x ./alternatives ] || exit 1
+# Include the BeakerLib environment
+. /usr/share/beakerlib/beakerlib.sh
 
-altdir=$(mktemp -d)
-admindir=$(mktemp -d)
-testdir=$(mktemp -d)
+# Set the full test name
+TEST="Test Alternatives"
 
-rc=0
-buf=
-
-name="TEST"
-link="${testdir}/main_link"
-
-sname="STEST"
-slink="${testdir}/slave_link"
+# Package being tested
+PACKAGE="chkconfig"
 
 function clean_dir {
     [ -n ${altdir} -a -d ${altdir} ] && rm ${altdir}/* &> /dev/null
@@ -42,8 +36,7 @@ function add_alternative {
     spath="${testdir}/$1/$slave"
     touch ${spath}
 
-    ./alternatives --altdir ${altdir} --admindir ${admindir} --install ${link} ${name} ${path} ${prio} --slave ${slink} ${sname} ${spath} ${family}
-    echo -e "NEW\tlink: $1\tPrio: $prio\tFamily: $3"
+    rlRun "./alternatives --altdir ${altdir} --admindir ${admindir} --install ${link} ${name} ${path} ${prio} --slave ${slink} ${sname} ${spath} ${family}" 0 "NEW\tlink: $1\tPrio: $prio\tFamily: $3"
 }
 
 function remove_alternative {
@@ -51,15 +44,34 @@ function remove_alternative {
     rm ${testdir}/$1/*
     rmdir ${testdir}/$1
 
-    ./alternatives --altdir ${altdir} --admindir ${admindir} --remove ${name} ${path}
-    echo -e "REMOVE\tlink: $1"
+    rlRun "./alternatives --altdir ${altdir} --admindir ${admindir} --remove ${name} ${path}" 0 "REMOVE\tlink: $1"
 }
 
 function set_alternative {
     path="${testdir}/$1/main"
 
-    ./alternatives --altdir ${altdir} --admindir ${admindir} --set ${name} ${path}
-    echo -e "SET\tlink: $1"
+    rlRun "./alternatives --altdir ${altdir} --admindir ${admindir} --set ${name} ${path}" 0 "SET\tlink: $1"
+}
+
+function add_slave {
+    path="${testdir}/$1/main"
+    slave=slave
+    touch $path
+
+    [ -n "$2" ] && slave=${2}
+
+    spath="${testdir}/$1/$slave"
+    touch ${spath}
+    rlRun "./alternatives --altdir ${altdir} --admindir ${admindir} --add-slave ${name} ${path} ${slink} ${sname} ${spath}" 0 "NEW_SLAVE\tlink: $spath"
+}
+
+function remove_slave {
+    path="${testdir}/$1/main"
+    slave=slave
+    touch $path
+
+    [ -n "$2" ] && slave=${2}
+    rlRun "./alternatives --altdir ${altdir} --admindir ${admindir} --remove-slave ${name} ${path} ${sname}" 0 "NEW_SLAVE\tlink: $spath"
 }
 
 function check_alternative {
@@ -76,134 +88,190 @@ function check_alternative {
          best=${path}
     fi
 
-    [ -n "$1" ] && slave=$1
+    if [ "$1" = EMPTY ] ; then
+        slave=
+    elif [ -n "$1" ] ; then
+        slave=$1
+    fi
 
     cur_path=$(readlink ${altdir}/${name} | xargs dirname | xargs basename)
     cur_state=$(head -1 ${admindir}/${name})
     cur_best=$(LC_ALL=C ./alternatives --altdir ${altdir} --admindir ${admindir} --display TEST | grep best | cut -d " " -f5 | sed -e 's/\.$//' | xargs dirname | xargs basename)
     cur_spath=$(readlink ${altdir}/${sname} | xargs basename)
     echo $cur_spath
-    echo
-    echo -n "Path:  ${cur_path}"
-    echo -e "\t(expected: ${path})"
-    echo -n "State: ${cur_state}"
-    echo -e "\t(expected: ${state})"
-    echo -n "Best: ${cur_best}"
-    echo -e "\t(expected: ${best})"
-    echo -n "Slave: ${cur_spath}"
-    echo -e "\t(expected: ${slave})"
-    if [ "${state}" = "${cur_state}" ] && [ "${best}" = "${cur_best}" ] &&  [ "${path}" = "${cur_path}" ] && [ "${cur_spath}" = "${slave}" ] ; then
-        echo -en '\e[0;32m'
-        echo -n "SUCCESS"
-        echo -e '\e[0m'
-    else
-        echo -en '\e[0;31m'
-        echo -n "FAIL"
-        echo -e '\e[0m'
-        ((rc++))
-    fi
-    echo
-    echo "-----------------------------------"
-    echo
+    rlAssertEquals "Mode:" "${state}" "${cur_state}"
+    rlAssertEquals "Highest Priority:" "${best}" "${cur_best}"
+    rlAssertEquals "Selected:" "${path}" "${cur_path}"
+    rlAssertEquals "Slave:" "${cur_spath}" "${slave}"
 }
 
-add_alternative link_a 10 ""
-check_alternative link_a auto
-clean_dir
+rlJournalStart
+    # Setup phase: Prepare test directory
+    rlPhaseStartSetup
+        rlAssertRpm $PACKAGE
+        rlRun 'altdir=$(mktemp -d)' 0 'Creating tmp directory' # no-reboot
+        rlRun 'admindir=$(mktemp -d)' 0 'Creating tmp directory' # no-reboot
+        rlRun 'testdir=$(mktemp -d)' 0 'Creating tmp directory' # no-reboot
 
-add_alternative link_a 10 ""
-set_alternative link_a
-check_alternative link_a manual link_a
-clean_dir
+        rlRun 'name="TEST"'
+        rlRun 'link="${testdir}/main_link"'
 
-add_alternative link_a 10 ""
-add_alternative link_b  20
-check_alternative link_b auto
-clean_dir
+        rlRun 'sname="STEST"'
+        rlRun 'slink="${testdir}/slave_link"'
+    rlPhaseEnd
 
-add_alternative link_a 20 ""
-add_alternative link_b 10 ""
-check_alternative link_a auto
-clean_dir
+    # Test phase: Testing touch, ls and rm commands
+    rlPhaseStart FAIL "Create Alternative"
+        add_alternative link_a 10 ""
+        check_alternative link_a auto
+        clean_dir
+    rlPhaseEnd
+        
+    rlPhaseStart FAIL "Set Manual"
+        add_alternative link_a 10 ""
+        set_alternative link_a
+        check_alternative link_a manual link_a
+        clean_dir
+    rlPhaseEnd
+        
+    rlPhaseStart FAIL "Auto Priority Ascendant"
+        add_alternative link_a 10 ""
+        add_alternative link_b  20
+        check_alternative link_b auto
+        clean_dir
+    rlPhaseEnd
+        
+    rlPhaseStart FAIL "Auto Priority Descendant"
+        add_alternative link_a 20 ""
+        add_alternative link_b 10 ""
+        check_alternative link_a auto
+        clean_dir
+    rlPhaseEnd
+        
+    rlPhaseStart FAIL "Manual Overrides Best"
+        add_alternative link_a 10 ""
+        set_alternative link_a
+        add_alternative link_b  20 ""
+        check_alternative link_a manual link_b
+        clean_dir
+    rlPhaseEnd
+        
+    rlPhaseStart FAIL "Remove Manually Set"
+        add_alternative link_a 10 ""
+        set_alternative link_a
+        add_alternative link_b  20 ""
+        remove_alternative link_a
+        check_alternative link_b auto
+        clean_dir
+    rlPhaseEnd
+        
+    rlPhaseStart FAIL "Slave"
+        add_alternative link_a 10 "" slave_a
+        add_alternative link_a 10 "" slave_b
+        check_alternative link_a auto slave_b
+        clean_dir
+    rlPhaseEnd
+        
+    rlPhaseStart FAIL "Slave Manual"
+        add_alternative link_a 10 "" slave_a
+        set_alternative link_a
+        add_alternative link_a 10 "" slave_b
+        check_alternative link_a manual link_a slave_b
+        clean_dir
+    rlPhaseEnd
+        
+        ##########
+        
+    rlPhaseStart FAIL "Family Priority Ascendant"
+        add_alternative link_a 10 family_a
+        add_alternative link_b 20 family_a
+        check_alternative link_b auto
+        clean_dir
+    rlPhaseEnd
+        
+    rlPhaseStart FAIL "Family Priority Descendant"
+        add_alternative link_a 20 family_a
+        add_alternative link_b 10 family_a
+        check_alternative link_a auto
+        clean_dir
+    rlPhaseEnd
+        
+    rlPhaseStart FAIL "Families Priority Ascendant"
+        add_alternative link_a 10 family_a
+        add_alternative link_b 20 family_b
+        check_alternative link_b auto
+        clean_dir
+    rlPhaseEnd
+        
+    rlPhaseStart FAIL "Families Priority Descendant"
+        add_alternative link_a 20 family_a
+        add_alternative link_b 10 family_b
+        check_alternative link_a auto
+        clean_dir
+    rlPhaseEnd
+        
+    rlPhaseStart FAIL "Families Priority Ascendant Multiple"
+        add_alternative link_a 10 family_a
+        add_alternative link_b 20 family_a
+        add_alternative link_c 30 family_b
+        check_alternative link_c auto
+        clean_dir
+    rlPhaseEnd
+        
+    rlPhaseStart FAIL "Families Remove Manually Set"
+        add_alternative link_a 10 family_a
+        set_alternative link_a
+        add_alternative link_c 30 family_b
+        remove_alternative link_a
+        check_alternative link_c auto
+        clean_dir
+    rlPhaseEnd
+        
+    rlPhaseStart FAIL "Families Remove Link After Manually Set Multiple"
+        add_alternative link_a 10 family_a
+        set_alternative link_a
+        add_alternative link_b 20 family_a
+        add_alternative link_c 30 family_b
+        remove_alternative link_a
+        check_alternative link_b manual link_c
+        clean_dir
+    rlPhaseEnd
+        
+    rlPhaseStart FAIL "Family After Remove Manually Set"
+        add_alternative link_a 10 ""
+        set_alternative link_a
+        add_alternative link_b 20 ""
+        add_alternative link_c 30 family_a
+        remove_alternative link_a
+        check_alternative link_c auto
+        clean_dir
+    rlPhaseEnd
 
-add_alternative link_a 10 ""
-set_alternative link_a
-add_alternative link_b  20 ""
-check_alternative link_a manual link_b
-clean_dir
+        ##########
 
-add_alternative link_a 10 ""
-set_alternative link_a
-add_alternative link_b  20 ""
-remove_alternative link_a
-check_alternative link_b auto
-clean_dir
+    rlPhaseStart FAIL "Dynamic Slave Add"
+        add_alternative link_a 10 ""
+        add_slave link_a slave_a
+        check_alternative link_a auto slave_a
+        clean_dir
+    rlPhaseEnd
 
-add_alternative link_a 10 "" slave_a
-add_alternative link_a 10 "" slave_b
-check_alternative link_a auto slave_b
-clean_dir
+    rlPhaseStart FAIL "Dynamic Slave Remove"
+        add_alternative link_a 10 ""
+        add_slave link_a slave_a
+        remove_slave link_a slave_a
+        check_alternative link_a auto EMPTY
+        clean_dir
+    rlPhaseEnd
 
-add_alternative link_a 10 "" slave_a
-set_alternative link_a
-add_alternative link_a 10 "" slave_b
-check_alternative link_a manual link_a slave_b
-clean_dir
+    rlPhase
 
-##########
+    # Cleanup phase: Remove test directory
+    rlPhaseStartCleanup
+        rlRun "rmdir $altdir $testdir $admindir"
+    rlPhaseEnd
+rlJournalEnd
 
-add_alternative link_a 10 family_a
-add_alternative link_b 20 family_a
-check_alternative link_b auto
-clean_dir
+# Print the test report
+rlJournalPrintText
 
-add_alternative link_a 20 family_a
-add_alternative link_b 10 family_a
-check_alternative link_a auto
-clean_dir
-
-add_alternative link_a 10 family_a
-add_alternative link_b 20 family_b
-check_alternative link_b auto
-clean_dir
-
-add_alternative link_a 20 family_a
-add_alternative link_b 10 family_b
-check_alternative link_a auto
-clean_dir
-
-add_alternative link_a 10 family_a
-add_alternative link_b 20 family_a
-add_alternative link_c 30 family_b
-check_alternative link_c auto
-clean_dir
-
-add_alternative link_a 10 family_a
-set_alternative link_a
-add_alternative link_c 30 family_b
-remove_alternative link_a
-check_alternative link_c auto
-clean_dir
-
-add_alternative link_a 10 family_a
-set_alternative link_a
-add_alternative link_b 20 family_a
-add_alternative link_c 30 family_b
-remove_alternative link_a
-check_alternative link_b manual link_c
-clean_dir
-
-add_alternative link_a 10 ""
-set_alternative link_a
-add_alternative link_b 20 ""
-add_alternative link_c 30 family_a
-remove_alternative link_a
-check_alternative link_c auto
-clean_dir
-
-
-echo "$rc fails"
-
-rmdir $altdir $testdir $admindir
-
-exit $rc
